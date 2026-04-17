00ac844128ae43bed993103392cdbed3e3314e4b
SimoneZoroddu <lenviur@gmail.com>
2026-04-17 15:04:01 +0200

    Bonus

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,21 +5,21 @@ const index = (req, res) => {
 
     const indexSql = `SELECT * FROM posts`
 
-    connection.query(indexSql, (err, results) =>{
+    connection.query(indexSql, (err, results) => {
         res.json(results)
     })
 
 }
 
 //DESTROY
-const destroy = (req,res) => {
+const destroy = (req, res) => {
 
     const id = parseInt(req.params.id)
 
     const destroySql = `DELETE FROM posts WHERE id = ?`
 
-    connection.query(destroySql, [id], (err, results) =>{
-        if (err) return res.status(500).json({ error: 'Failed to delete Post'})
+    connection.query(destroySql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Failed to delete Post' })
         res.sendStatus(204)
     })
 }
@@ -27,13 +27,36 @@ const destroy = (req,res) => {
 
 const show = (req, res) => {
 
-    const {id} = req.params
+    const { id } = req.params
 
     const showSql = `SELECT * FROM posts WHERE id = ?`
 
-    connection.query(showSql, [id], (err, results) => {
-        if (err) return res.status(500).json({ error: 'Post non trovato'})
-        res.json(results[0])
+    const tagsSql = `
+    SELECT T.*
+    FROM tags AS T
+    JOIN post_tag AS PT ON T.id = PT.tag_id
+    WHERE PT.post_id = ?
+    `
+
+    connection.query(showSql, [id], (err, showResults) => {
+        if (err) return res.status(500).json({ error: 'Post non trovato' })
+
+        const singlePost = showResults[0];
+
+        //console.log(singlePost);
+
+        connection.query(tagsSql, [id], (err, tagsResults) => {
+            if (err) return res.status(500).json({ error: 'database query error' })
+
+            console.log(tagsResults);
+
+            singlePost.label = tagsResults
+            res.json(singlePost)
+
+        })
+
+
+
     })
 
 }
